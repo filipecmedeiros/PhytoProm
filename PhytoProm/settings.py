272bf4170c4312c657898efe3855ff38293437b9
Filipe Medeiros <fcma@cin.ufpe.br>
2018-08-21 20:10:27 -0300

    Adicionando form para análise

--- PhytoProm/settings.py
+++ PhytoProm/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #libs
     'whitenoise',
+    'widget_tweaks',
     #Apps
     'core',
     'transcriptors',
--- core/views.py
+++ core/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
 
+from log.forms import AnalysisForm
+from log.models import Log
 # Create your views here.
-def index (request):
-	return render(request, 'index.html')
+class Index (FormView):
+	template_name = 'index.html'
+
+	def get(self, request, *args, **kwargs):
+		self.context = {'genes':Log.objects.filter(gene='Vigun01g000200')}
+		print (self.context)
+		self.success = True
+		form = AnalysisForm(self.request.GET or None)
+		if form.is_valid():
+			self.success = True
+			self.context = Log.objects.filter(gene__icontains=form.cleaned_data['query'])[:10]
+		
+		return self.render_to_response(self.get_context_data(form=form))
+
+	def get_context_data(self, **kwargs):
+		context = super(Index, self).get_context_data(**kwargs)
+		context.update({
+			'context':self.context
+			})
+		return context
+	
+index = Index.as_view()

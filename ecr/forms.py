32996c2a7c02a00541f9c6c5b3783996dd527eb7
Filipe Azevedo <fcma@cin.ufpe.br>
2018-10-04 15:28:45 -0300

    Meetup data science

--- ecr/forms.py
+++ ecr/forms.py
@@ -4,7 +4,7 @@ import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-from .models import Specie, Log, Background
+from .models import Specie, Log, Background, Transcriptor
 
 
 class AnalyzeForm(forms.Form):
@@ -18,22 +18,37 @@ class AnalyzeForm(forms.Form):
         cutoff = self.cleaned_data['cutoff']
         cluster = cluster.split('\r\n')
 
-        queryset = Log.objects.filter(promoter_id__in=cluster)
         df = pd.DataFrame(list(Background.objects.all().values(
             'id', 'name', 'family', 'motifs', 'reverseComplement', 'vigna_genome')))
         df['cluster'] = 0
-        analyze = pd.DataFrame(list(queryset.values(
+
+        queryset = Log.objects.filter(promoter_id__in=cluster)
+        log = pd.DataFrame(list(queryset.values(
             'promoter_id', 'tf', 'upstream', 'downstream', 'mean', 'sumatory')))
-        i = analyze.groupby('promoter_id').count()
-        
-        #df['cluster'] = i['sumatory']
+
+        enrichment = log.groupby('tf').sum().reset_index()
+
+        tf = Transcriptor.objects.all().exclude(id__in=enrichment.tf)
+        tf = pd.DataFrame(list(tf.values('id')))
+                                
+        tf.rename(columns={'id':'tf'}, inplace=True)
+        tf['mean'] = 0
+        tf['sumatory'] = 0
+
+        enrichment = pd.concat([enrichment, tf]).reset_index()
+        enrichment = enrichment.sort_values(by='tf')
+        df = df.sort_values(by='id')
+        print (enrichment)
+        df['cluster'] = enrichment['sumatory']
+        print(df)
 
 
         context = {
             'success': True,
+            'dataframe' : df.to_html(classes=['table', 'table-striped', 'table-bordered', 'table-hover']),
             'specie': self.cleaned_data['specie'],
             'table': list(queryset),
-            'dataframe': df.to_html(classes=['table', 'table-striped', 'table-bordered', 'table-hover']),
+            'log': log.to_html(classes=['table', 'table-striped', 'table-bordered', 'table-hover']),
         }
 
         return context

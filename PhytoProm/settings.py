9126693f7385e7ae5a4bf9494ab75210c1c0d560
Filipe Medeiros <fcma@cin.ufpe.br>
2019-02-07 22:20:40 -0300

    Deploying app on Heroku

--- PhytoProm/settings.py
+++ PhytoProm/settings.py
@@ -120,9 +120,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-]
+)
 
 #Heroku settings
 DATABASES = { 
